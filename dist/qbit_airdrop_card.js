0392c8bc4017f5c1491142e7f024e5e485a55c14
Joseph Brandenburg <inveniofirmamentum@gmail.com>
2026-06-25 20:27:07 -0700

    Update qbit_airdrop_card.js

--- dist/qbit_airdrop_card.js
+++ dist/qbit_airdrop_card.js
@@ -328,7 +328,8 @@ function formatUp(bps){
           .down{
             text-align:right;
             font-variant-numeric: tabular-nums;
-            font-size:calc(1em - 1.4pt);
+            font-size:calc(1em - 1.6pt);
+			line-height:1.2;
             color:#16ba3f;
             white-space:nowrap; overflow:hidden; text-overflow:clip;
           }
@@ -339,7 +340,6 @@ function formatUp(bps){
             font-weight:400;
             font-size:calc(1em - 1pt);
             line-height:1.2;
-            align-self:end;
             color:#ebbf10;
             white-space:nowrap;
             overflow:hidden;
@@ -355,7 +355,7 @@ function formatUp(bps){
             font-variant-numeric:tabular-nums;
             font-size:calc(1em - 1pt);
             line-height:1.2;
-			padding: 0px 5px;
+			padding: 0px 0px 0px 5px;
             color:#12c5de;
             white-space:nowrap; overflow:hidden; text-overflow:clip;
             cursor:pointer;
