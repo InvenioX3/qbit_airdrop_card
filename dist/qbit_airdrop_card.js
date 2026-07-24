6901187c89a2dee39f84027dfe7442d6015db0ca
Joseph Brandenburg <inveniofirmamentum@gmail.com>
2026-07-23 18:58:06 -0700

    Update qbit_airdrop_card.js

--- dist/qbit_airdrop_card.js
+++ dist/qbit_airdrop_card.js
@@ -407,10 +407,10 @@ function formatSeeds(num_seeds, num_complete){
             padding-top:2px;
           }
           .sticky-spacer{
-            height:15px;
+            height:20px;
             max-width:360px;
             width:100%;
-			background: linear-gradient(to bottom, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%) !important;
+			background: linear-gradient(to bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%) !important;
             padding:0;
             margin:0 auto;
           }
